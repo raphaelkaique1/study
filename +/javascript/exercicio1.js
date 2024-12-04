78520b9c46b630c2dd3477a7c1ab69f22fff9a4e
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2024-12-04 08:49:11 -0300

    up to date

--- +/javascript/exercicio1.js
+++ +/javascript/exercicio1.js
@@ -9,3 +9,19 @@ var potencia = (a, b) => {
 }
 
 console.log(potencia(5, 2))
+
+doces = [{sabor: "morango"}, "pudim", "manjar", {}]
+
+doces[3].sabor = "maracuja"
+
+function vs(){
+    console.log(doces[0].sabor) // morango
+    console.log(doces[3]) // maracujá (sabor att to obj{})
+    doces[1] = "goiabada"
+}
+
+console.log(doces[1]) // pudim
+
+vs()
+
+// pudim morango { sabor: 'maracuja' }
