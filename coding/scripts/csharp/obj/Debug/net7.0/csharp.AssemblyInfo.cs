ddde55f77dd401d8e4ed849469a3bcbbca23f493
raphaelkaique1 <raphaelkaiquediassantos1@gmail.com>
2025-05-04 19:23:03 -0300

    feat: update BACKEND NODEJS

--- coding/scripts/csharp/obj/Debug/net7.0/csharp.AssemblyInfo.cs
+++ coding/scripts/csharp/obj/Debug/net7.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9201819c90b12cdd3cbee871fe89f42c7c825e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f9404667e9c93f1db30571cd1adf1aed9184df")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
